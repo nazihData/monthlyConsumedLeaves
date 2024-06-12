cb89eb5c23a0501e8b2b52cfd4ee6635c1cea2a6
nazihData <140287198+nazihData@users.noreply.github.com>
2024-06-12 22:36:22 +0300

    Update MonthlyConsumedLeaves.py

--- MonthlyConsumedLeaves.py
+++ MonthlyConsumedLeaves.py
@@ -4,6 +4,7 @@ import base64
 import io
 import openpyxl
 
+
 # # Function to create download link for DataFrame
 def get_download_link(df, label):
     # Reset index if MultiIndex columns are present
@@ -21,10 +22,10 @@ def get_download_link(df, label):
 # Main Streamlit app
 def main():
     st.set_page_config(page_title="Monthly Consumed Leaves")
-    st.markdown("<h1 style='text-align: center; font-size: 45px;'>All Types Report</h1>", unsafe_allow_html=True)
-    st.title("Upload Nazih Leaves")
+    st.markdown("<h1 style='text-align: left; font-size: 35px;'>Monthly Consumed Leaves Report</h1>", unsafe_allow_html=True)
+
   
-    uploaded_file = st.file_uploader("Choose a file", type=["xlsx"])
+    uploaded_file = st.file_uploader("Upload Monthly Report Leaves Data as .xlsx from discoverer", type=["xlsx"])
 
     if uploaded_file is not None:
         try:
@@ -56,15 +57,11 @@ def main():
             # Group by Sector, Absense type, and year_month to count days
             test = expanded_df[(expanded_df['Absense type'] != 'أجازة عارضة') & (expanded_df['Absense type'] != 'أجازة اعتيادية')]
 
-            # result = expanded_df.groupby(['Sector', 'Absense type', 'year_month']).size().reset_index(name='days_count')
-
             pivot_table1 = pd.pivot_table(test, index=['Sector'], columns=['year_month', 'Absense type'], aggfunc='size', fill_value=0)
-            # Suppose pivot_table is your DataFrame with multi-index columns
-            pivot_table1.reset_index(inplace=True)
 
-            pivot_table2 = pd.pivot_table(expanded_df, index=['Sector', 'Absense type'], columns='year_month', aggfunc='size', fill_value=0)
+            pivot_table2 = pd.pivot_table(test, index=['Sector', 'Absense type'], columns='year_month', aggfunc='size', fill_value=0)
 
-            pivot_table3 = pd.pivot_table(expanded_df, index=['year_month', 'Absense type'], columns='Sector', aggfunc='size', fill_value=0)
+            pivot_table3 = pd.pivot_table(test, index=['year_month', 'Absense type'], columns='Sector', aggfunc='size', fill_value=0)
 
             # Add labels for each download link
             download_links = {
@@ -79,6 +76,59 @@ def main():
             for label, data in download_links.items():
                 download_link = get_download_link(data, label)
                 st.markdown(download_link, unsafe_allow_html=True)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+
+    uploaded_file2 = st.file_uploader("Upload Annual leaves Report from discoverer", type=["xlsx"])
+
+    if uploaded_file2 is not None:
+        try:
+            df2 = pd.read_excel(uploaded_file2)
+            st.write("Uploaded DataFrame:")
+            st.write(df2)
+            
+            # Data processing
+            df2 = df2[~df2['Category meaning'].str.contains('-', na=False)]
+            df2 = df2.dropna(subset=['Full Name'])
+            df2 = df2[df2['Person Employmnt Type'] != 'Ex-employee']
+            df2['Sector'] = df2.apply(lambda x: x['Central Department'] if '-' in str(x['Sector']) else x['Sector'], axis=1)
+            df2['Required Date Starting'] = pd.to_datetime(df2['Required Date Starting'])
+            df2['Required Date Ending'] = pd.to_datetime(df2['Required Date Ending'])
+
+            # Create a DataFrame with a row for each day in the range for each employee
+            date_ranges2 = []
+            for _, row2 in df2.iterrows():
+                sector2 = row2['Sector']
+                start_date2 = row2['Required Date Starting']
+                end_date2 = row2['Required Date Ending']
+                absence_type2 = row2['Absense type']
+                for single_date2 in pd.date_range(start_date2, end_date2):
+                    date_ranges2.append({'Sector': sector2, 'Absense type': absence_type2, 'date': single_date2})
+
+            expanded_df2 = pd.DataFrame(date_ranges2)
+            expanded_df2['year_month'] = expanded_df2['date'].dt.strftime('%m %B')
+
+            # Group by Sector, Absense type, and year_month to count days
+            test2 = expanded_df2[(expanded_df2['Absense type'] == 'أجازة عارضة') | (expanded_df2['Absense type'] == 'أجازة اعتيادية')]
+
+            result = test2.groupby(['Sector', 'year_month'], as_index=False).size()
+
+            pivot_table12 = pd.pivot_table(result, index=['Sector'], columns=['year_month'], aggfunc='sum', fill_value=0)
+
+
+            # Add labels for each download link
+            download_links2 = {
+                'Download Report - Sectors Annual leaves consumption': pivot_table12,
+
+                # Add more download links as needed
+            }
+            # Download button
+            # Display download links
+            for label2, data2 in download_links2.items():
+                download_link2 = get_download_link(data2, label2)
+                st.markdown(download_link2, unsafe_allow_html=True)
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
